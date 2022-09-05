34dbaccb482e42ea16a6f4fe1a89f19a63660779
flozo.home <github.mail@flozo.de>
2022-09-05 14:30:15 +0200

    Add Thymeleaf commands to list.html

--- src/main/java/com/example/spring5webapp/domain/Book.java
+++ src/main/java/com/example/spring5webapp/domain/Book.java
@@ -80,7 +80,6 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", publisher=" + publisher +
-                ", authors=" + authors +
                 '}';
     }
 
